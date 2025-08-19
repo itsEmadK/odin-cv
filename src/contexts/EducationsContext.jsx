fbfb46818f6e2e7542457cbf79dc4663ba7095be
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-08-19 18:00:46 +0330

    Add educations context

---
+++ src/contexts/EducationsContext.jsx
@@ -0,0 +1,79 @@
+import { createContext, useContext, useMemo, useReducer } from 'react';
+import { person as sample } from '../person';
+
+const EducationsContext = createContext();
+const EducationsApiContext = createContext();
+
+const initialEducations = sample.educations;
+
+export const useEducations = () => useContext(EducationsContext);
+export const useEducationsApi = () => useContext(EducationsApiContext);
+
+export default function EducationsProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialEducations);
+  const api = useMemo(() => {
+    return {
+      addEducation: (education) => {
+        dispatch({ type: 'addEducation', education });
+      },
+      removeEducation: (id) => {
+        dispatch({ type: 'removeEducation', id });
+      },
+      updateEducation: (education) => {
+        dispatch({ type: 'updateEducation', education });
+      },
+      setEducations: (educations) => {
+        dispatch({ type: 'setEducations', educations });
+      },
+      clear: () => {
+        dispatch({ type: 'clear' });
+      },
+      loadDefaults: () => {
+        dispatch({ type: 'loadDefault' });
+      },
+    };
+  }, []);
+
+  return (
+    <EducationsContext.Provider value={state}>
+      <EducationsApiContext.Provider value={api}>{children}</EducationsApiContext.Provider>
+    </EducationsContext.Provider>
+  );
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'addEducation': {
+      const newId = state.educations.length > 0 ? state.educations.at(-1).id + 1 : 0;
+      return { ...state, educations: [...state.educations, { ...action.education, id: newId }] };
+    }
+    case 'removeEducation': {
+      return { ...state, educations: state.educations.filter((e) => e.id !== action.id) };
+    }
+    case 'setEducations': {
+      return { ...state, educations: action.educations };
+    }
+    case 'updateEducation': {
+      return {
+        ...state,
+        educations: [
+          ...state.educations.filter((e) => e.id !== action.education.id),
+          action.education,
+        ],
+      };
+    }
+    case 'clear': {
+      return {
+        ...state,
+        educations: [],
+      };
+    }
+    case 'loadDefaults': {
+      return { ...state, educations: initialEducations };
+    }
+
+    default: {
+      throw new Error(`${action.type} is not supported.`);
+    }
+  }
+}
