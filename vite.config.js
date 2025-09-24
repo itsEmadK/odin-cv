c61fab679090ffa7b492787a2c1cd64706598923
itsEmadK <itsEmadK@users.noreply.github.com>
2025-09-24 20:15:48 +0000

    deploy: 4c9e1200dfc1641e0ca070295c218270f407fdae

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-const REPO_NAME = 'odin-cv';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: `/${REPO_NAME}/`, //Comment this out if you want to host this project anywhere other than GitHub
-});
